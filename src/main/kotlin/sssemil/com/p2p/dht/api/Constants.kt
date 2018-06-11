76b6c2ea7cc9a65a9605cf25a8013592a4b23c4e
Emil Suleymanov <emil.suleymanov@tum.de>
2018-06-11 03:16:19 +0200

    Add new API codes

--- src/main/kotlin/sssemil/com/p2p/dht/api/Constants.kt
+++ src/main/kotlin/sssemil/com/p2p/dht/api/Constants.kt
@@ -1,9 +1,16 @@
 package sssemil.com.p2p.dht.api
 
 internal const val DHT_PUT = 650.toShort()
-internal const val DHT_GET = 651.toShort() // this will be used by other modules
-internal const val DHT_GET_INTERNAL = 652.toShort() // this will be used among DHT modules only
+internal const val DHT_GET = 651.toShort()
 internal const val DHT_SUCCESS = 652.toShort()
 internal const val DHT_FAILURE = 653.toShort()
+
+internal const val DHT_PING = 654.toShort()
+internal const val DHT_PONG = 655.toShort()
+internal const val DHT_FIND_NODE = 656.toShort()
+internal const val DHT_FIND_VALUE = 657.toShort()
+internal const val DHT_ERROR = 658.toShort()
+internal const val DHT_NODE = 659.toShort()
+internal const val DHT_VALUE = 660.toShort()
 
 internal const val KEY_LENGTH = 32
