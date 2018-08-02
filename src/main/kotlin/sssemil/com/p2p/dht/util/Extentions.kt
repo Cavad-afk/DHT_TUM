f40747798512ba6f5b7efd54c6ec710773e1bf31
Emil Suleymanov <emil.suleymanov@tum.de>
2018-08-02 20:50:42 +0200

    Some ByteArray ext

--- src/main/kotlin/sssemil/com/p2p/dht/util/Extentions.kt
+++ src/main/kotlin/sssemil/com/p2p/dht/util/Extentions.kt
@@ -2,6 +2,8 @@ package sssemil.com.p2p.dht.util
 
 import java.io.DataOutputStream
 import java.net.Socket
+import java.nio.charset.Charset
+import java.util.*
 
 fun DataOutputStream.writeShort(value: Short) {
     writeShort(value.toInt())
@@ -27,3 +29,12 @@ val Socket.isAlive: Boolean
     get() {
         return isBound && isConnected && !isClosed && !isInputShutdown && !isOutputShutdown
     }
+
+
+fun ByteArray.str(): String {
+    return Base64.getDecoder().decode(this).toString(Charset.defaultCharset())
+}
+
+fun ByteArray.toBase64(): String {
+    return Base64.getEncoder().encode(this).toString(Charset.defaultCharset())
+}
