d62ae26af5d62d447681d9a40f975853c321bfc0
Emil Suleymanov <emil.suleymanov@tum.de>
2018-08-03 01:09:27 +0200

    Add janitor for recurring tasks

---
+++ src/main/kotlin/sssemil/com/p2p/dht/Janitor.kt
@@ -0,0 +1,44 @@
+package sssemil.com.p2p.dht
+
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import sssemil.com.p2p.dht.api.DEFAULT_REPLICATION
+import sssemil.com.p2p.dht.api.DhtObj
+import sssemil.com.p2p.dht.api.OBJ_PUT
+import sssemil.com.p2p.dht.api.model.Put
+import sssemil.com.p2p.dht.util.Logger
+
+/**
+ * Janitor performs repeating maintenance tasks.
+ */
+class Janitor(private val server: Server, private val client: Client) {
+    fun start() = async {
+        delay(MINUTE)
+
+        Storage.cleanup()
+
+        val all = Storage.getAll()
+
+        Logger.i("[JANITOR] Resending all data.")
+
+        all.forEach { key, value ->
+            val dhtObj = DhtObj(OBJ_PUT, Put((
+                    value.ttl - (System.currentTimeMillis() - value.arrivedAt)).toShort(),
+                    DEFAULT_REPLICATION, value.value))
+            if ((dhtObj.obj as Put).ttl > 0) {
+                client.send(dhtObj)
+            }
+        }
+
+        Logger.i("[JANITOR] Resending all data - completed.")
+
+        delay(HOUR)
+    }
+
+    companion object {
+        const val SECOND = 1000
+        const val MINUTE = 60 * SECOND
+        const val HOUR = 60 * MINUTE
+        const val DAY = 24 * HOUR
+    }
+}
