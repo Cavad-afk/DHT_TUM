dea7a7f6cac5127eb58fbe75a72ba5dc30846338
Emil Suleymanov <emil.suleymanov@tum.de>
2018-08-03 01:10:16 +0200

    Add cleanup & entry struc to storage

--- src/main/kotlin/sssemil/com/p2p/dht/Storage.kt
+++ src/main/kotlin/sssemil/com/p2p/dht/Storage.kt
@@ -1,12 +1,38 @@
 package sssemil.com.p2p.dht
 
+import java.util.*
+
 class Storage {
 
+    data class Entry(val value: ByteArray, val ttl: Short, val arrivedAt: Long) {
+        fun isOutdated() = System.currentTimeMillis() - arrivedAt > ttl
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Entry
+
+            if (!Arrays.equals(value, other.value)) return false
+            if (ttl != other.ttl) return false
+            if (arrivedAt != other.arrivedAt) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = Arrays.hashCode(value)
+            result = 31 * result + ttl
+            result = 31 * result + arrivedAt.hashCode()
+            return result
+        }
+    }
+
     companion object {
-        private val hashMap = HashMap<ByteArray, ByteArray>()
+        private val hashMap = HashMap<ByteArray, Entry>()
 
         fun store(key: ByteArray, value: ByteArray, ttl: Short) {
-            hashMap[key] = value
+            hashMap[key] = Entry(value, ttl, System.currentTimeMillis())
         }
 
         fun remove(key: ByteArray): Boolean {
@@ -22,7 +48,18 @@ class Storage {
         fun contains(key: ByteArray) = hashMap.contains(key)
 
         fun get(key: ByteArray) = if (contains(key)) {
-            hashMap[key]
+            hashMap[key]?.value
         } else null
+
+        fun cleanup() {
+            val keys = hashMap.keys
+            keys.forEach { key ->
+                if (hashMap[key]?.isOutdated() == true) {
+                    hashMap.remove(key)
+                }
+            }
+        }
+
+        fun getAll() = hashMap
     }
 }
