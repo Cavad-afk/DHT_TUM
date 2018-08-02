5f8dfbd8ebe9dd27ff6c23022224b3071b8634a4
Emil Suleymanov <emil.suleymanov@tum.de>
2018-08-03 01:10:49 +0200

    Use BigIntegers for xor

--- src/main/kotlin/sssemil/com/p2p/dht/util/IdKeyUtils.kt
+++ src/main/kotlin/sssemil/com/p2p/dht/util/IdKeyUtils.kt
@@ -3,7 +3,6 @@ package sssemil.com.p2p.dht.util
 import com.google.common.math.BigIntegerMath
 import java.math.BigInteger
 import java.math.RoundingMode
-import kotlin.experimental.xor
 
 class IdKeyUtils {
 
@@ -14,19 +13,9 @@ class IdKeyUtils {
                 throw RuntimeException("Can't work with differently sized arrays!")
             }
 
-            val array = ByteArray(first.size)
+            val result = BigInteger(first).xor(BigInteger(second)).abs()
 
-            for (i in array.indices) {
-                array[i] = first[i] xor second[i]
-            }
-
-            var bigInt = BigInteger(array)
-
-            if (bigInt < BigInteger.ZERO) {
-                bigInt = bigInt.negate()
-            }
-
-            return if (bigInt == BigInteger.ZERO) return 0 else BigIntegerMath.log2(bigInt, RoundingMode.FLOOR)
+            return if (result == BigInteger.ZERO) return 0 else BigIntegerMath.log2(result, RoundingMode.HALF_DOWN)
         }
     }
 }
