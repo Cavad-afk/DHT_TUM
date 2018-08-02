235fd91de01b2f1334775ecaf0aa7b0ff721bfb7
Emil Suleymanov <emil.suleymanov@tum.de>
2018-08-03 01:08:59 +0200

    Add equals and hashCode to models

--- src/main/kotlin/sssemil/com/p2p/dht/api/model/Pong.kt
+++ src/main/kotlin/sssemil/com/p2p/dht/api/model/Pong.kt
@@ -1,3 +1,23 @@
 package sssemil.com.p2p.dht.api.model
 
-data class Pong(val peerId: ByteArray, val port: Int) : TokenModel()
+import java.util.*
+
+data class Pong(val peerId: ByteArray, val port: Int) : TokenModel() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Pong
+
+        if (!Arrays.equals(peerId, other.peerId)) return false
+        if (port != other.port) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = Arrays.hashCode(peerId)
+        result = 31 * result + port
+        return result
+    }
+}
--- src/main/kotlin/sssemil/com/p2p/dht/api/model/Put.kt
+++ src/main/kotlin/sssemil/com/p2p/dht/api/model/Put.kt
@@ -1,8 +1,29 @@
 package sssemil.com.p2p.dht.api.model
 
 import sssemil.com.p2p.dht.util.generateKey
+import java.util.*
 
 data class Put(val ttl: Short, var replicationsLeft: Byte, val value: ByteArray) : TokenModel() {
     val key: ByteArray
         get() = generateKey(value)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Put
+
+        if (ttl != other.ttl) return false
+        if (replicationsLeft != other.replicationsLeft) return false
+        if (!Arrays.equals(value, other.value)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = ttl.toInt()
+        result = 31 * result + replicationsLeft
+        result = 31 * result + Arrays.hashCode(value)
+        return result
+    }
 }
