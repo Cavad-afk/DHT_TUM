2b02426ab15ffdd0a7c7e69f7dbe489207f69675
Emil Suleymanov <emil.suleymanov@tum.de>
2018-08-02 23:27:11 +0200

    Rewrite API codes

--- src/main/kotlin/sssemil/com/p2p/dht/api/Constants.kt
+++ src/main/kotlin/sssemil/com/p2p/dht/api/Constants.kt
@@ -6,17 +6,17 @@ internal const val DHT_SUCCESS = 652.toShort()
 internal const val DHT_FAILURE = 653.toShort()
 internal const val DHT_OBJ = 654.toShort()
 
-internal const val DHT_PING = 1
-internal const val DHT_PONG = 2
-internal const val DHT_FIND_NODE = 3
-internal const val DHT_FIND_VALUE = 4
-internal const val DHT_ERROR = 5
-internal const val DHT_NODE = 6
-internal const val DHT_VALUE = 7
+internal const val OBJ_PING = 1
+internal const val OBJ_PONG = 2
+internal const val OBJ_PUT = 3
+internal const val OBJ_FIND_VALUE = 4
+internal const val OBJ_FOUND_VALUE = 5
+internal const val OBJ_FOUND_PEERS = 6
 
 internal const val KEY_LENGTH = 16 // bytes
-internal const val TOLERANCE = 15 // bytes
+internal const val TOLERANCE = 15 * 8 // distance
 internal const val BUCKETS = 20
+internal const val GRAB_SIZE = 3
 
 internal const val DEFAULT_DELAY = 10000 // 5s
 internal const val PING_DELAY = 5000 // 5s
