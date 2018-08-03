def247f050ff9d7181487a8f246dacda9b5e9ae0
Emil Suleymanov <emil.suleymanov@tum.de>
2018-08-03 22:56:11 +0200

    Move more to functions in main

--- src/main/kotlin/sssemil/com/p2p/dht/Main.kt
+++ src/main/kotlin/sssemil/com/p2p/dht/Main.kt
@@ -1,5 +1,6 @@
 package sssemil.com.p2p.dht
 
+import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.runBlocking
 import sssemil.com.p2p.dht.api.*
 import sssemil.com.p2p.dht.api.model.Pong
@@ -43,129 +44,22 @@ fun main(args: Array<String>) {
                             printHelp()
                         }
                         "ping" -> {
-                            if (parts.size == 3) {
-                                try {
-                                    val pingIp = InetAddress.getByName(parts[1])
-                                    val pingPort = parts[2].toInt()
-                                    if (pingPort < 2000 || pingPort > 2100) {
-                                        throw NumberFormatException()
-                                    }
-
-                                    val client = Client(pingIp, pingPort)
-
-                                    if (!client.connect().await()) {
-                                        Logger.i("Connection failed!")
-                                    } else {
-                                        Logger.i("ping ${pingIp.hostAddress} $pingPort : ${client.ping(server.thisPeerId, server.port).await()}")
-                                    }
-                                } catch (e: NumberFormatException) {
-                                    Logger.e("Invalid port number!")
-                                } catch (e: UnknownHostException) {
-                                    Logger.e("Invalid IP!")
-                                }
-                            } else {
-                                printPingHelp()
-                            }
+                            ping(parts, server)
                         }
                         "list" -> {
-                            server.peersStorage.forEachIndexed { i, bucket ->
-                                bucket.forEachIndexed { j, peer ->
-                                    Logger.i("$i $j $peer")
-                                }
-                            }
+                            list(parts, server)
                         }
                         "addPeer" -> {
-                            if (parts.size == 3 || parts.size == 4) {
-                                try {
-                                    val peerIp = InetAddress.getByName(parts[1])
-                                    val peerPort = parts[2].toInt()
-                                    if (peerPort < 2000 || peerPort > 2100) {
-                                        throw NumberFormatException()
-                                    }
-
-                                    val client = Client(peerIp, peerPort)
-
-                                    if (!client.connect().await()) {
-                                        Logger.i("Connection failed!")
-                                    } else {
-                                        val pong = client.ping(server.thisPeerId, server.port).await() as DhtObj?
-                                        Logger.i("ping ${peerIp.hostAddress} $peerPort : $pong")
-
-                                        pong?.let {
-                                            var valid = true
-                                            val factualPeerId = (it.obj as Pong).peerId
-                                            if (parts.size == 4) {
-                                                val providedId = parts[3].hexStringToByteArray()
-
-                                                if (!providedId.contentEquals(factualPeerId)) {
-                                                    Logger.e("Peer verification failed! ${providedId.toHexString()} != ${factualPeerId.toHexString()}.")
-                                                    valid = false
-                                                }
-                                            }
-
-                                            if (valid) {
-                                                val peer = Peer(factualPeerId, peerIp, peerPort)
-                                                Logger.i("Adding new peer: $peer")
-                                                server.addPeer(peer)
-                                            }
-                                        }
-                                    }
-                                } catch (e: NumberFormatException) {
-                                    Logger.e("Invalid port number!")
-                                } catch (e: UnknownHostException) {
-                                    Logger.e("Invalid IP!")
-                                }
-                            } else {
-                                printAddPeerHelp()
-                            }
+                            addPeer(parts, server)
                         }
                         "put" -> {
-                            if (parts.size > 1) {
-                                val value = line.substring(line.indexOf(" ") + 1).toByteArray()
-                                val key = generateKey(value)
-
-                                Logger.i("Here is your key: ${key.toHexString()}. Attempting saving.")
-
-                                val dhtPut = DhtPut(DEFAULT_TTL, DEFAULT_REPLICATION, key, value)
-                                selfClient.send(dhtPut)
-                            } else {
-                                printPutHelp()
-                            }
+                            put(parts, selfClient)
                         }
                         "get" -> {
-                            if (parts.size == 2) {
-                                val key = parts[1]
-
-                                Logger.i("FindValue key: $key. Searching...")
-
-                                val dhtGet = DhtGet(Base64.getDecoder().decode(key))
-
-                                val response = selfClient.send(dhtGet,
-                                        {
-                                            it is DhtSuccess || it is DhtFailure
-                                        },
-                                        DEFAULT_DELAY).await()
-
-                                Logger.i("FindValue response: $response")
-                            } else {
-                                printGetHelp()
-                            }
+                            get(parts, selfClient)
                         }
                         "port" -> {
-                            if (parts.size == 2) {
-                                try {
-                                    val newPort = parts[1].toInt()
-
-                                    server.port = newPort
-                                    server.stop().await()
-                                    server.start().await()
-                                } catch (e: NumberFormatException) {
-                                    Logger.e(e.localizedMessage)
-                                    printPortHelp()
-                                }
-                            } else {
-                                Logger.i("Current port: $server.port")
-                            }
+                            port(parts, server)
                         }
                         "exit" -> {
                             System.exit(0)
@@ -174,6 +68,137 @@ fun main(args: Array<String>) {
                 }
             }
         }
+    }
+}
+
+fun port(parts: List<String>, server: Server) = async {
+    if (parts.size == 2) {
+        try {
+            val newPort = parts[1].toInt()
+
+            server.port = newPort
+            server.stop().await()
+            server.start().await()
+        } catch (e: NumberFormatException) {
+            Logger.e(e.localizedMessage)
+            printPortHelp()
+        }
+    } else {
+        Logger.i("Current port: $server.port")
+    }
+}
+
+fun get(parts: List<String>, selfClient: Client) = async {
+    if (parts.size == 2) {
+        val key = parts[1]
+
+        Logger.i("FindValue key: $key. Searching...")
+
+        val dhtGet = DhtGet(Base64.getDecoder().decode(key))
+
+        val response = selfClient.send(dhtGet,
+                {
+                    it is DhtSuccess || it is DhtFailure
+                },
+                DEFAULT_DELAY).await()
+
+        Logger.i("FindValue response: $response")
+    } else {
+        printGetHelp()
+    }
+}
+
+fun put(parts: List<String>, client: Client) {
+    if (parts.size > 1) {
+        val value = parts.joinToString(" ").toByteArray()
+        val key = generateKey(value)
+
+        Logger.i("Here is your key: ${key.toHexString()}. Attempting saving.")
+
+        val dhtPut = DhtPut(DEFAULT_TTL, DEFAULT_REPLICATION, key, value)
+        client.send(dhtPut)
+    } else {
+        printPutHelp()
+    }
+}
+
+fun addPeer(parts: List<String>, server: Server) = async {
+    if (parts.size == 3 || parts.size == 4) {
+        try {
+            val peerIp = InetAddress.getByName(parts[1])
+            val peerPort = parts[2].toInt()
+            if (peerPort < 2000 || peerPort > 2100) {
+                throw NumberFormatException()
+            }
+
+            val client = Client(peerIp, peerPort)
+
+            if (!client.connect().await()) {
+                Logger.i("Connection failed!")
+            } else {
+                val pong = client.ping(server.thisPeerId, server.port).await() as DhtObj?
+                Logger.i("ping ${peerIp.hostAddress} $peerPort : $pong")
+
+                pong?.let {
+                    var valid = true
+                    val factualPeerId = (it.obj as Pong).peerId
+                    if (parts.size == 4) {
+                        val providedId = parts[3].hexStringToByteArray()
+
+                        if (!providedId.contentEquals(factualPeerId)) {
+                            Logger.e("Peer verification failed! ${providedId.toHexString()} != ${factualPeerId.toHexString()}.")
+                            valid = false
+                        }
+                    }
+
+                    if (valid) {
+                        val peer = Peer(factualPeerId, peerIp, peerPort)
+                        Logger.i("Adding new peer: $peer")
+                        server.addPeer(peer)
+                    }
+                }
+            }
+        } catch (e: NumberFormatException) {
+            Logger.e("Invalid port number!")
+        } catch (e: UnknownHostException) {
+            Logger.e("Invalid IP!")
+        }
+    } else {
+        printAddPeerHelp()
+    }
+}
+
+fun list(parts: List<String>, server: Server) {
+    server.peersStorage.forEachIndexed { i, bucket ->
+        bucket.forEachIndexed { j, peer ->
+            Logger.i("$i $j $peer")
+        }
+    }
+}
+
+fun ping(parts: List<String>, server: Server) = async {
+    if (parts.size == 3) {
+        try {
+            val pingIp = InetAddress.getByName(parts[1])
+            val pingPort = parts[2].toInt()
+            if (pingPort < 2000 || pingPort > 2100) {
+                throw NumberFormatException()
+            }
+
+            val client = Client(pingIp, pingPort)
+
+            if (!client.connect().await()) {
+                Logger.i("Connection failed!")
+            } else {
+                Logger.i("ping ${pingIp.hostAddress} $pingPort : ${client.ping(server.thisPeerId, server.port).await()}")
+            }
+        } catch (e: NumberFormatException) {
+            Logger.e("Invalid port number!")
+        } catch (e: UnknownHostException) {
+            Logger.e("Invalid IP!")
+        }
+    } else {
+        printPingHelp()
     }
 }
 
